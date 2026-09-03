bc4c4f644f1d0f0854b9d7abb9acc4324d507dd4
Mohammed Almohmdy <160524171+mohammedalmohmdy@users.noreply.github.com>
2026-09-04 04:22:34 +0530

    Delete AFR_FineGrained_SBIR directory

--- AFR_FineGrained_SBIR/scripts/train_qmul.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python train.py --config configs/afrsbir_qmul_shoe.yaml
--- AFR_FineGrained_SBIR/utils/schedulers.py
+++
@@ -1,7 +0,0 @@
-
-import torch
-
-def build_scheduler(optimizer, name="step"):
-    if name == "cosine":
-        return torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=50)
-    return torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
